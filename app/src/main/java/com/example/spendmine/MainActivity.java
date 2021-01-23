65c59dec0dfb05b2b179934bf4158b2b1d37cdcb
Manish <mangreat64@gmail.com>
2021-01-23 20:59:28 +0530

    Add Splash screen

---
+++ app/src/main/java/com/example/spendmine/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.spendmine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(MainActivity.this, Login.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_TIME);
+
+    }
+}
