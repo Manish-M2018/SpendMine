595f66d1ba38f6d1d6faf1d24354eaa942929f3b
Manish <mangreat64@gmail.com>
2021-01-24 12:08:13 +0530

    Add Fluffy coins fragment

--- app/src/main/java/com/example/spendmine/Dashboard.java
+++ app/src/main/java/com/example/spendmine/Dashboard.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.spendmine.fragments.Business_Profile;
+import com.example.spendmine.fragments.Fluffy_coins;
 import com.example.spendmine.fragments.Student_Profile;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -49,6 +50,9 @@ public class Dashboard extends AppCompatActivity {
                 fragment = new Student_Profile();
                 break;
 
+            case R.id.coins:
+                fragment = new Fluffy_coins();
+                break;
             case R.id.logout:
                 final Dialog myDialog;
                 myDialog = new Dialog(this);
